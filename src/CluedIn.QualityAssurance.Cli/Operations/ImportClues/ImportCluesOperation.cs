6e9b698bd949bfb39a0b2a0fe2ae35a982b978ab
andrew-hardy <aha@cluedin.com>
2022-11-16 22:15:20 +1000

    feat: Add git version azure pipelines (#1)

--- src/CluedIn.QualityAssurance.Cli/Operations/ImportClues/ImportCluesOperation.cs
+++ src/CluedIn.QualityAssurance.Cli/Operations/ImportClues/ImportCluesOperation.cs
@@ -1,20 +1,20 @@
-﻿using CommandLine;
-using Microsoft.Extensions.Logging;
+﻿//using CommandLine;
+//using Microsoft.Extensions.Logging;
 
-namespace CluedIn.QualityAssurance.Cli.Operations.ImportClues;
+//namespace CluedIn.QualityAssurance.Cli.Operations.ImportClues;
 
-[Verb("import-clues", HelpText = "Import serialized clues")]
-internal class ImportCluesOperation : Operation<ImportCluesOptions>
-{
-    private readonly ILogger<ImportCluesOperation> _logger;
+//[Verb("import-clues", HelpText = "Import serialized clues")]
+//internal class ImportCluesOperation : Operation<ImportCluesOptions>
+//{
+//    private readonly ILogger<ImportCluesOperation> _logger;
 
-    public ImportCluesOperation(ILogger<ImportCluesOperation> logger)
-    {
-        _logger = logger;
-    }
+//    public ImportCluesOperation(ILogger<ImportCluesOperation> logger)
+//    {
+//        _logger = logger;
+//    }
 
-    public override Task ExecuteAsync(ImportCluesOptions options, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
-}
+//    public override Task ExecuteAsync(ImportCluesOptions options, CancellationToken cancellationToken)
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
