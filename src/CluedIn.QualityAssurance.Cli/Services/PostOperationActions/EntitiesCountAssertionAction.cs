853b1d9ff6092d3ea32fede5359a0cf6df50fbef
Ho Peng Foong <pfh@cluedin.com>
2023-08-11 13:32:45 +0800

    feat: Insert entity counts from different datastore to output (#18)
    AB#24787

--- src/CluedIn.QualityAssurance.Cli/Services/PostOperationActions/EntitiesCountAssertionAction.cs
+++ src/CluedIn.QualityAssurance.Cli/Services/PostOperationActions/EntitiesCountAssertionAction.cs
@@ -1,7 +1,15 @@
-﻿using CluedIn.QualityAssurance.Cli.Environments;
+﻿using CluedIn.Core.Accounts;
+using CluedIn.QualityAssurance.Cli.Environments;
+using CluedIn.QualityAssurance.Cli.Models.ElasticSearch;
+using CluedIn.QualityAssurance.Cli.Models.Neo4j;
 using CluedIn.QualityAssurance.Cli.Models.Operations;
+using CluedIn.QualityAssurance.Cli.Models.SqlServer;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Neo4j.Driver;
+using Neo4jClient;
+using System.Net.Http.Json;
+using System.Text;
 
 namespace CluedIn.QualityAssurance.Cli.Services.PostOperationActions;
 
@@ -18,69 +26,207 @@ internal class EntitiesCountAssertionAction : IPostOperationAction
 
     public async Task ExecuteAsync(SingleIterationOperationResult result, CancellationToken cancellationToken)
     {
-        var neo4jConnectionInfo = await TestEnvironment.GetNeo4jConnectionInfoAsync(cancellationToken);
-        var driver = GraphDatabase.Driver(neo4jConnectionInfo.BoltUri, AuthTokens.Basic(neo4jConnectionInfo.UserName, neo4jConnectionInfo.Password));
+        var neo4jConnectionInfo = await TestEnvironment.GetNeo4jConnectionInfoAsync(cancellationToken).ConfigureAwait(false);
+        var elasticSearchConnectionInfo = await TestEnvironment.GetElasticSearchConnectionInfoAsync(cancellationToken).ConfigureAwait(false);
+        var sqlServerConnectionInfo = await TestEnvironment.GetSqlServerConnectionInfoAsync(cancellationToken).ConfigureAwait(false);
 
-        using var session = driver.AsyncSession();
-        await GetEntityCountAsync(result, session).ConfigureAwait(false);
-        await GetEntityTypesCountAsync(result, session).ConfigureAwait(false);
+        var organizationId = result.Organization.OrganizationId;
+        var entityTypes = await GetEntityTypesAsync(organizationId, neo4jConnectionInfo).ConfigureAwait(false);
+
+        var neo4jResultTask = GetResultForNeo(organizationId, entityTypes, neo4jConnectionInfo);
+        var elasticSearchResultTask = GetResultForElasticSearch(organizationId, entityTypes, elasticSearchConnectionInfo);
+        var sqlServerResultTask = GetResultForSql(organizationId, entityTypes, sqlServerConnectionInfo);
+
+        await Task.WhenAll(neo4jResultTask, elasticSearchResultTask, sqlServerResultTask).ConfigureAwait(false);
+
+        var neoResult = await neo4jResultTask;
+        var esResult = await elasticSearchResultTask;
+        var sqlResult = await sqlServerResultTask;
+
+        result.Output.Add("EntitiesTypesCount.Neo4j", neoResult);
+        result.Output.Add("EntitiesTypesCount.ElasticSearch", esResult);
+        result.Output.Add("EntitiesTypesCount.SqlServer", sqlResult);
     }
 
-    private async Task GetEntityCountAsync(SingleIterationOperationResult result, IAsyncSession session)
+    private async Task<ICollection<string>> GetEntityTypesAsync(Guid organizationId, Neo4jConnectionInfo connectionInfo)
     {
-        var query = @$"MATCH (n:Entity)
-                            WHERE n.Organization = '{result.Organization.OrganizationId}'
-                            RETURN COUNT(*) AS Count";
+        var entityTypes = new List<string>();
+        var client = new BoltGraphClient(connectionInfo.BoltUri, connectionInfo.UserName, connectionInfo.Password);
+        await client.ConnectAsync();
+        var labelsQuery = client.Cypher.Call("db.labels()").Yield("label");
+        var labelsResultQuery = labelsQuery.Return<string?>("label");
 
-        var entitiesCount = await session.ExecuteReadAsync(async tx =>
+        var labels = (await labelsResultQuery.ResultsAsync).ToList();
+        var entityTypeLabelsOfInterest = labels.Where(currentLabel => currentLabel.StartsWith("/") && !currentLabel.StartsWith("/Temporal"));
+
+        foreach (var currentLabel in entityTypeLabelsOfInterest)
         {
-            var result = await tx.RunAsync(query, new Dictionary<string, object>()).ConfigureAwait(false);
-            return await result.SingleAsync(record => record.Values["Count"] as long? ?? 0).ConfigureAwait(false);
-        }).ConfigureAwait(false);
+            var match = $"(n:`{currentLabel}`:Entity)";
+            var firstEntityQuery = client.Cypher.Match(match)
+                .Where($"n.`Organization` = '{organizationId}'");
+            var firstEntityResultQuery = firstEntityQuery.Return<int?>("1").Limit(1);
+            var firstEntityResult = (await firstEntityResultQuery.ResultsAsync).SingleOrDefault();
 
-        result.Output.Add("EntitiesCount", entitiesCount);
-    }
-
-    private async Task GetEntityTypesCountAsync(SingleIterationOperationResult result, IAsyncSession session)
-    {
-        var query = @$"MATCH (n:Entity)
-                            WHERE n.Organization = '{result.Organization.OrganizationId}'
-                            RETURN DISTINCT labels(n) AS Labels, COUNT(*) AS Count";
-
-        var records = await session.ExecuteReadAsync(async tx =>
-        {
-            var result = await tx.RunAsync(query, new Dictionary<string, object>()).ConfigureAwait(false);
-            return await result.ToListAsync(record =>
+            if (firstEntityResult != null)
             {
-                var labels = record.Values["Labels"] as IEnumerable<object>;
-                if (labels == null)
-                {
-                    return null;
-                }
-                var possibleTypes = labels
-                                .Select(label => label as string)
-                                .Where(label => label.StartsWith("/"))
-                                .OrderBy(label => label);
-                if (possibleTypes.Count() == 0)
-                {
-                    Logger.LogWarning("Unable to find entity type using labels {Labels} because none starts with '/'.", possibleTypes);
-                    return null;
-                }
+                entityTypes.Add(currentLabel);
+            }
+        }
 
-                return new
-                {
-                    Types = possibleTypes,
-                    Count = record.Values["Count"] as long? ?? 0
-                };
-            }).ConfigureAwait(false);
-        }).ConfigureAwait(false);
-
-        var entityTypes = records
-            .Where(record => record != null)
-            .SelectMany(record => record.Types.Select(type => new EntityTypeCount(type, record.Count)))
-            .ToList();
-        result.Output.Add("EntitiesTypesCount", entityTypes);
+        return entityTypes;
     }
 
-    public record EntityTypeCount(string EntityType, long Count);
+    private async Task<Dictionary<string, EntityTypeCount>> GetResultForSql(Guid organizationId, ICollection<string> entityTypes, SqlServerConnectionInfo connectionInfo)
+    {
+        var result = new Dictionary<string, EntityTypeCount>();
+        var builder = new SqlConnectionStringBuilder();
+
+        builder.DataSource = $"{connectionInfo.Host},{connectionInfo.Port}";
+        builder.UserID = connectionInfo.UserName;
+        builder.Password = connectionInfo.Password;
+        builder.InitialCatalog = "DataStore.Db.BlobStorage";
+        builder.TrustServerCertificate = true;
+
+        using (var connection = new SqlConnection(builder.ConnectionString))
+        {
+            await connection.OpenAsync();
+            var grandTotal = 0;
+            foreach (var currentEntityType in entityTypes)
+            {
+                var sql = "SELECT COUNT(*) FROM Blobs WHERE OrganizationId = @OrganizationId AND [Name] LIKE @Name";
+
+
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "OrganizationId",
+                        Value = organizationId,
+                    });
+                    command.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "Name",
+                        Value = $"{currentEntityType}%",
+                    });
+                    var count = await command.ExecuteScalarAsync() as int?;
+                    if (count == null)
+                    {
+                        throw new InvalidOperationException("Error trying to execute query to get entity count.");
+                    }
+                    result.Add(currentEntityType, new(count.Value, count.Value));
+                }
+            }
+        }
+        return result;
+    }
+
+    private async Task<Dictionary<string, EntityTypeCount>> GetResultForNeo(Guid organizationId, ICollection<string> entityTypes, Neo4jConnectionInfo connectionInfo)
+    {
+        var result = new Dictionary<string, EntityTypeCount>();
+        var client = new BoltGraphClient(connectionInfo.BoltUri, connectionInfo.UserName, connectionInfo.Password);
+        await client.ConnectAsync();
+
+        foreach (var currentEntityType in entityTypes)
+        {
+            var nonShadowQuery = client.Cypher.Match($"(n:`{currentEntityType}` {{Organization: '{organizationId}', IsShadowEntity: 'False'}})");
+            var nonShadowResultQuery = nonShadowQuery.Return<long?>("count(n)");
+            var nonShadow = (await nonShadowResultQuery.ResultsAsync).SingleOrDefault();
+            var totalQuery = client.Cypher.Match($"(n:`{currentEntityType}` {{Organization: '{organizationId}'}})");
+            var totalResultQuery = totalQuery.Return<long?>("count(n)");
+            var total = (await totalResultQuery.ResultsAsync).SingleOrDefault();
+
+            result.Add(currentEntityType, new(nonShadow.Value, total.Value));
+        }
+        return result;
+    }
+
+    private async Task<Dictionary<string, EntityTypeCount>> GetResultForElasticSearch(Guid organizationId, ICollection<string> entityTypes, ElasticSearchConnectionInfo connectionInfo)
+    {
+        var result = new Dictionary<string, EntityTypeCount>();
+        foreach (var currentEntityType in entityTypes)
+        {
+            var client = new HttpClient();
+            var nonShadowRequest = new HttpRequestMessage(HttpMethod.Get, new Uri(connectionInfo.ServerUri, "_count"));
+            var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{connectionInfo.UserName}:{connectionInfo.Password}"));
+            nonShadowRequest.Headers.Add("Authorization", $"Basic {credentials}");
+
+            var nonShadowBody = $@"
+            {{
+              ""query"" : {{
+                ""bool"": {{
+                    ""must"": [
+                        {{
+                            ""term"" : {{ ""organizationId"" : ""{organizationId}"" }}
+                        }},
+                        {{
+                            ""term"" : {{ ""entityType"" : ""{currentEntityType}"" }}
+                        }},
+                        {{
+                            ""term"" : {{ ""isShadowEntity"" : false }}
+                        }}
+                    ]
+                }} 
+              }}
+            }}";
+            nonShadowRequest.Content = new StringContent(nonShadowBody, null, "application/json");
+            var nonShadowResponse = await client.SendAsync(nonShadowRequest);
+            nonShadowResponse.EnsureSuccessStatusCode();
+            var nonShadowResult = await nonShadowResponse.Content.ReadFromJsonAsync<Result>();
+
+            var withShadowRequest = new HttpRequestMessage(HttpMethod.Get, new Uri(connectionInfo.ServerUri, "_count"));
+            withShadowRequest.Headers.Add("Authorization", $"Basic {credentials}");
+
+            var withShadowBody = $@"
+            {{
+              ""query"" : {{
+                ""bool"": {{
+                    ""must"": [
+                        {{
+                            ""term"" : {{ ""organizationId"" : ""{organizationId}"" }}
+                        }},
+                        {{
+                            ""term"" : {{ ""entityType"" : ""{currentEntityType}"" }}
+                        }}
+                    ]
+                }} 
+              }}
+            }}";
+            withShadowRequest.Content = new StringContent(withShadowBody, null, "application/json");
+            var withShadowResponse = await client.SendAsync(withShadowRequest);
+            withShadowResponse.EnsureSuccessStatusCode();
+            var withShadowResult = await withShadowResponse.Content.ReadFromJsonAsync<Result>();
+
+            result.Add(currentEntityType, new(nonShadowResult.count, withShadowResult.count));
+        }
+        return result;
+    }
+
+
+
+    //public class EntityResult
+    //{
+    //    public EntityResult(long nonShadowCount, long totalCount)
+    //    {
+    //        this.nonShadowCount = nonShadowCount;
+    //        this.totalCount = totalCount;
+    //    }
+    //    public long nonShadowCount { get; set; }
+    //    public long totalCount { get; set; }
+    //}
+
+    public class Result
+    {
+        public long count { get; set; }
+        public Shard _shards { get; set; }
+    }
+
+    public class Shard
+    {
+        public int total { get; set; }
+        public int successful { get; set; }
+        public int skipped { get; set; }
+        public int failed { get; set; }
+    }
+
+    public record EntityTypeCount(long NonShadowCount, long TotalCount);
 }
