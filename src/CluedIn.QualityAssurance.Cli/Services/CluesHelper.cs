bf16bbe57558d004009fb06a11ee51768b2d450e
Ho Peng Foong <pfh@cluedin.com>
2023-10-24 15:26:34 +0800

    refactor: Simplify code

--- src/CluedIn.QualityAssurance.Cli/Services/CluesHelper.cs
+++ src/CluedIn.QualityAssurance.Cli/Services/CluesHelper.cs
@@ -30,13 +30,8 @@ internal class CluesHelper
         var edgeNodes = xmlDoc.SelectNodes("//edge")?.OfType<XmlNode>() ?? Array.Empty<XmlNode>();
         foreach (var node in edgeNodes)
         {
-            var fromAttribute = node?.Attributes?["from"];
-            var toAttribute = node?.Attributes?["to"];
-
-            if (fromAttribute == null || toAttribute == null)
-            {
-                throw new InvalidOperationException("From or To Attribute is null.");
-            }
+            var fromAttribute = node?.Attributes?["from"] ?? throw new InvalidOperationException("From Attribute is null.");
+            var toAttribute = node?.Attributes?["to"] ?? throw new InvalidOperationException("To Attribute is null.");
             fromAttribute.Value += idSuffix;
             toAttribute.Value += idSuffix;
         }
@@ -44,7 +39,8 @@ internal class CluesHelper
         var originNodes = xmlDoc.SelectNodes("//*[@origin]")?.OfType<XmlNode>() ?? Array.Empty<XmlNode>();
         foreach (var node in originNodes)
         {
-            node.Attributes["origin"].Value += idSuffix;
+            var originAttribute = node?.Attributes?["origin"] ?? throw new InvalidOperationException("Origin Attribute is null.");
+            originAttribute.Value += idSuffix;
         }
 
         xml = xmlDoc.OuterXml;
